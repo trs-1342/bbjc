b87e927c746598dd48eb32d155e37dd793e43378
trs-1342 <socialmediaoftrs1342@gmail.com>
2025-08-11 20:22:51 +0300

    dnm for vercel

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Projects from "./components/Projects";
+import Products from "./components/Products";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Projects />
+      <Products />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
